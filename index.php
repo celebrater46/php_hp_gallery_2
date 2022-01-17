424fbc6031162123186b1d5153db877addc00f9b
celebrater <tkgm45nbchk@gmail.com>
2022-01-18 01:17:27 +0900

    created view.php

--- index.php
+++ index.php
@@ -1,31 +1,11 @@
 <?php
 
-$test_url = "test";
+$test_url = 1;
+$max_thumb = 16;
 
-// hoge.png, "title", "title_en", "description", "description_en"
-$images_array = [
-    [
-        "201223-sally4c.png",
-        "Merry X'mas",
-        "Merry X'mas",
-        "小説「白金記」よりサリーさんがクリスマスプレゼントを持ってきてくれたようです。",
-        "Sally might give you a certain gift as X'mas present."
-    ],
-    [
-        "201230-sarma-color-t.png",
-        "Happy New Year 2021",
-        "Happy New Year 2021",
-        "小説「第三世界収容所」より、罰の天使サーマさんです。",
-        'Her name is "Sarma". She is the angel of punishment in my novel "The 3rd Prison".'
-    ],
-    [
-        "210804-hizuruSeifukuRedC.jpg",
-        "ヒヅル様がセーラー服姿を披露されたようです",
-        "That suits Hizuru-sama",
-        "年甲斐もなk……おっと、誰か来たようだ。",
-        'Hizuru-sama is wearing a sailor suit. Praise her.'
-    ],
-];
+require_once("info.php");
+
+$count = count($images_array);
 
 function h($s) {
     return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
@@ -36,22 +16,26 @@ function h($s) {
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <meta name="Author" content="Enin Fujimi - 富士見永人">
-    <title>Dark Labyrinth Gallery</title>
-    <link rel="stylesheet" href="css/style.css">
+    <meta name="Author" content="Enin Fujimi">
+    <title>PHP MY GALLERY</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/gallery.css">
+    <link rel="shortcut icon" href="favicon.ico">
 </head>
 <body>
-    <h1>
+    <h1 class="gallery">
         <a href="/?t=<?php echo h($test_url); ?>">
-            Dark Labyrinth Gallery
+            PHP MY GALLERY
         </a>
     </h1>
     <div class="container">
-        <?php foreach ($images_array as $item) : ?>
+        <?php for ($i = 0; $i < $count; $i++) : ?>
             <div class="thumb_box">
-                <img src="<?php echo "img/".$item[0]; ?>">
+                <a href="view.php?id=<?php echo h($i); ?>">
+                    <img src="<?php echo "img/".$images_array[$i]["src"]; ?>">
+                </a>
             </div>
-        <?php endforeach; ?>
+        <?php endfor; ?>
     </div>
 
 </body>
