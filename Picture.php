044c1e741632a0a055ddf899f72d74c017597407
celebrater <tkgm45nbchk@gmail.com>
2022-01-29 00:46:51 +0900

    create Picture class

---
+++ Picture.php
@@ -0,0 +1,130 @@
+<?php
+
+class Picture
+{
+    public $id;
+    public $dir;
+    public $file_name;
+    public $thumb;
+    public $title;
+    public $comment;
+
+    function __construct($id, $line, $lang){
+        $temp_array = explode("|", $line);
+        $this->dir = "img/";
+        $this->id = $id;
+        $this->file_name = $this->check_img($temp_array[0]);
+        $this->thumb = $this->check_thumb();
+        $this->title = $this->get_name($temp_array, $lang);
+        $this->comment = $this->get_comment($temp_array, $lang);
+    }
+
+//    function rename_file_404(){
+//        $this->file_name = "../404.png";
+//    }
+
+    function get_name($array, $lang){
+        if(!(isset($array[1]))){
+            return "無題";
+        }
+        if($lang !== 1){
+            return $array[1];
+        } else {
+            if(isset($array[3])){
+                return $array[3];
+            } else{
+                return "UNKNOWN";
+            }
+        }
+    }
+
+    function get_comment($array, $lang){
+        if(!(isset($array[2]))){
+            return "コメントは特にありません。";
+        }
+        if($lang !== 1){
+            return $array[2];
+        } else {
+            if(isset($array[4])){
+                return $array[4];
+            } else{
+                return "No comment.";
+            }
+        }
+    }
+
+    function check_img($file){
+        if(strpos($file, ".png") === false && strpos($file, ".jpg") === false && strpos($file, ".gif") === false){
+            if(file_exists($this->dir . $file . ".png")){
+                return $file . ".png";
+            } elseif(file_exists($this->dir . $file . ".jpg")) {
+                return $file . ".jpg";
+            } elseif(file_exists($this->dir . $file . ".gif")) {
+                return $file . ".gif";
+            } else {
+                return "../404.png";
+            }
+        } else {
+            if(file_exists($this->dir . $file)){
+                return $file;
+            } else {
+                return "../404.png";
+            }
+        }
+    }
+
+    function check_thumb(){
+        if($this->file_name === "../404.png"){
+            $this->thumb = "../404.png";
+        } else {
+            if(file_exists("thumb/" . $this->file_name) === false){
+                $this->create_thumb($this->file_name);
+            }
+            return "thumb/" . $this->file_name;
+        }
+//        if($list === null){
+//            return null;
+//        } elseif($pictures === null){
+//            return null;
+//        } else {
+//            foreach ($pictures as $picture){
+//                $img = $this->check_img($this->file_name);
+//                if($img === null){
+//                    // 元の画像がない場合、404 画像割当
+//                    $this->rename_file_404();
+//                } else {
+//                    // サムネが存在しない場合、自動生成
+//                    if(file_exists("thumb/" . $img) === false){
+//                        create_thumb($img);
+//                        return "thumb/" . $img;
+//                    }
+//                }
+//            }
+//        }
+    }
+
+    function create_thumb($file){
+        $src = "img/" . $file;
+        $to = "thumb/" . $file;
+        $size = getimagesize($src); // [0] => x, [1] => y
+        $width = $size[0];
+        $height = $size[1];
+        $thumb = $this->calc_thumb_size($width, $height);
+
+        // コピー先リソース、コピー元リソース、コピー先のX座標、同Y座標、コピー元のX座標、Y座興、コピー先の幅、高さ、コピー元の幅、高さ
+        imagecopyresampled($to, $src, 0, 0, 0, 0, $thumb["x"], $thumb["y"], $width, $height);
+    }
+
+    function calc_thumb_size($x, $y){
+        $thumb_size = 400;
+        $array = ["x" => 0, "y" => 0];
+        if($x > $y){
+            $array["y"] = $thumb_size;
+            $array["x"] = round($x / $y * $thumb_size);
+        } else {
+            $array["x"] = $thumb_size;
+            $array["y"] = round($y / $x * $thumb_size);
+        }
+        return $array;
+    }
+}
--- index.php
+++ index.php
@@ -5,7 +5,144 @@ $max_thumb = 16;
 
 require_once("info.php");
 
-$count = count($images_array);
+//$count = count($images_array);
+//$thumb_size = 400;
+$lang = 0;
+$setting = get_setting();
+$list = get_list($setting);
+$pictures = get_pictures($list, $lang);
+check_thumb($list, $pictures); // サムネが存在するかチェックし、なければ生成する
+
+//function check_thumb($list, $pictures){
+//    if($list === null){
+//        return null;
+//    } elseif($pictures === null){
+//        return null;
+//    } else {
+//        foreach ($pictures as $picture){
+//            $img = check_img($picture->filename);
+//            if($img === null){
+//                // 元の画像がない場合、404 画像割当
+//                $picture->rename_file_404();
+//            } else {
+//                // サムネが存在しない場合、自動生成
+//                if(file_exists("thumb/" . $img) === false){
+//                    create_thumb($img);
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//function create_thumb($file){
+//    $src = "img/" . $file;
+//    $to = "thumb/" . $file;
+//    $size = getimagesize($src); // [0] => x, [1] => y
+//    $width = $size[0];
+//    $height = $size[1];
+////    $img_type = $width > $height ? "x" : "y"; // 横長なら x 縦長なら y
+//    $thumb = calc_thumb_size($width, $height);
+//
+//    // コピー先リソース、コピー元リソース、コピー先のX座標、同Y座標、コピー元のX座標、Y座興、コピー先の幅、高さ、コピー元の幅、高さ
+//    imagecopyresampled($to, $src, 0, 0, 0, 0, $thumb["x"], $thumb["y"], $width, $height);
+//}
+// 1200 800
+//function calc_thumb_size($x, $y){
+//    $thumb_size = 400;
+//    $array = ["x" => 0, "y" => 0];
+//    if($x > $y){
+//        $array["y"] = $thumb_size;
+//        $array["x"] = round($x / $y * $thumb_size);
+//    } else {
+//        $array["x"] = $thumb_size;
+//        $array["y"] = round($y / $x * $thumb_size);
+//    }
+//    return $array;
+//}
+
+//function check_img($file){
+//    $dir = "img/";
+//    if(strpos($file, ".png") === false && strpos($file, ".jpg") === false && strpos($file, ".gif") === false){
+//        if(file_exists($dir . $file . ".png")){
+//            return $file . ".png";
+//        } elseif(file_exists($dir . $file . ".jpg")) {
+//            return $file . ".jpg";
+//        } elseif(file_exists($dir . $file . ".gif")) {
+//            return $file . ".gif";
+//        } else {
+//            return null;
+//        }
+//    } else {
+//        if(file_exists($dir . $file)){
+//            return $file;
+//        } else {
+//            return null;
+//        }
+//    }
+//}
+
+function get_pictures($list, $lang){
+    if($list[0] === "no_list_mode"){
+        $file_names = glob("img/*");
+        $array = [];
+        $i = 0;
+        foreach ($file_names as $file){
+            $temp = str_replace("img/", "", $file);
+            array_push($array, new Picture($i, $temp . "|無題|コメントは特にありません。|UNKNOWN|No comment.", $lang));
+            $i++;
+        }
+        return $array;
+    } elseif ($list === null) {
+        return null;
+    } else {
+        $array = [];
+        $i = 0;
+        foreach ($list as $line){
+            $temp = str_replace(["\r", "\n", "\r\n"], "", $line);
+            array_push($array, new Picture($i, $temp, $lang));
+            $i++;
+        }
+        return $array;
+    }
+}
+
+function get_list($setting){
+    // title_and_comment
+    if($setting === null){
+        return null;
+    }
+    if(file_exists("list.txt")){
+        return file("list.txt");
+    } else {
+        if($setting[1] === "false"){
+            return ["no_list_mode"];
+        } else {
+//            return ["ERROR:", 'list.txt が見つかりません。Not found "list.txt".'];
+            return null;
+        }
+    }
+}
+
+//function get_img_file_names($list_line){
+//    if($list_line === "no_list_mode"){
+//        return glob("img/*");
+//    } else {
+//        return null;
+//    }
+//}
+
+function get_setting(){
+    if(file_exists("setting.txt")){
+        $setting = file("setting.txt");
+        $array = [];
+        foreach ($setting as $line){
+            array_push($array, str_replace(["multi_language:", "title_and_comment:", " ", "\r", "\n", "\r\n"], "", $line));
+        }
+        return $array;
+    } else {
+        return null;
+    }
+}
 
 function h($s) {
     return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
@@ -17,7 +154,7 @@ function h($s) {
 <head>
     <meta charset="utf-8">
     <meta name="Author" content="Enin Fujimi">
-    <title>PHP MY GALLERY</title>
+    <title>PHP HP GALLERY</title>
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/gallery.css">
     <link rel="shortcut icon" href="favicon.ico">
@@ -25,17 +162,28 @@ function h($s) {
 <body>
     <h1 class="gallery">
         <a href="/?t=<?php echo h($test_url); ?>">
-            PHP MY GALLERY
+            PHP HP GALLERY
         </a>
     </h1>
     <div class="container">
-        <?php for ($i = 0; $i < $count; $i++) : ?>
-            <div class="thumb_box">
-                <a href="view.php?id=<?php echo h($i); ?>">
-                    <img src="<?php echo "img/".$images_array[$i]["src"]; ?>">
-                </a>
-            </div>
-        <?php endfor; ?>
+        <?php if($setting === null): ?>
+            <p>ERROR: setting.txt が見つかりません。Not found "setting.txt"</p>
+        <?php elseif($list === null): ?>
+            <p>ERROR: list.txt が見つかりません。Not found "list.txt"</p>
+        <?php else: ?>
+            <?php if($file_names === null): ?>
+
+            <?php else: ?>
+
+            <?php endif ?>
+            <?php for ($i = 0; $i < $count; $i++) : ?>
+                <div class="thumb_box">
+                    <a href="view.php?id=<?php echo h($i); ?>">
+                        <img src="<?php echo "img/".$images_array[$i]["src"]; ?>">
+                    </a>
+                </div>
+            <?php endfor; ?>
+        <?php endif ?>
     </div>
 
 </body>
