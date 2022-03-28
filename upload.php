4d65c4f95c72312ed0b250f1a5e32adb0ff96a13
celebrater <tkgm45nbchk@gmail.com>
2022-03-29 02:57:33 +0900

    edited create_upload_button for php_hp_gallery

--- upload.php
+++ upload.php
@@ -7,6 +7,23 @@ use image_uploader as iu;
 require_once "init.php";
 require_once PHG_IU_DIR . "iu_get_html.php";
 
+function get_contents_in_form(){
+    return <<<EOT
+        <label for="category">カテゴリ(整数): </label>
+        <input class="text" type="text" id="category" name="category" size="3"/>　
+        <label for="title_jp">タイトル: </label>
+        <input class="text" type="text" id="title_jp" name="title_jp" size="15"/>
+        <br>
+        <br>
+        <label for="comment_jp">コメント: </label>
+        <br>
+        <textarea id="comment_jp" name="comment_jp" style="width: 640px; height: 320px"></textarea>
+        <br>
+        <br>
+EOT;
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -19,24 +36,12 @@ require_once PHG_IU_DIR . "iu_get_html.php";
 <body>
 <div class="container uploader">
     <h1>Upload Pictures</h1>
-    <form action="upload_result.php" method="post">
-        <label for="category">カテゴリ(整数): </label>
-        <input class="text" type="text" id="category" name="category" size="3"/>　
-        <label for="title_jp">タイトル: </label>
-        <input class="text" type="text" id="title_jp" name="title_jp" size="15"/>
-        <br>
-        <br>
-        <label for="comment_jp">コメント: </label>
-        <br>
-        <textarea id="comment_jp" name="comment_jp" style="width: 640px; height: 320px"></textarea>
-        <br>
-        <br>
-        <!--    <input type="text" id="path" name="path" size="3"/>-->
-        <!--    <input type="submit" value="Update!"/>-->
-        <?php echo iu\iu_get_html("画像ファイル："); ?>
-        <br>
-        <input class="btn" type="submit" value="送信">
-    </form>
+<!--    <form action="upload_result.php" method="post">-->
+<!--    <form action="" method="post" enctype="multipart/form-data" id="my-form">-->
+        <?php echo iu\iu_get_html(get_contents_in_form()); ?>
+<!--        <br>-->
+<!--        <input class="btn" type="submit" value="送信">-->
+<!--    </form>-->
 </div>
 </body>
 </html>
